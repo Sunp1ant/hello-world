ee0b33535c1f312aacad0b4e05bf29a9a5030bc4
Sunp1ant <63898278+Sunp1ant@users.noreply.github.com>
2020-04-18 16:09:13 +0800

    komiit

--- bg.js
+++ bg.js
@@ -1,16 +1,16 @@
 (function() {
-    var bodyElement = document.getElementsByTagName("BODY")[0].style;
+    var bodyElement = document.getElementsByTagName("BODY")[0];
     var swap = false;
     var color1 = '#80baf0';
     var color2 = '#fffdf0';
 
     setInterval(function() {
         if (swap){
-            bodyElement.backgroundColor = color1;
-            bodyElement.color = color1;
+            bodyElement.style.backgroundColor = color1;
+            bodyElement.style.color = color1;
         } else {
-            bodyElement.backgroundColor = color2;
-            bodyElement.color = color2;
+            bodyElement.style.backgroundColor = color2;
+            bodyElement.style.color = color2;
         }
         swap = !swap
     }, 1000);
