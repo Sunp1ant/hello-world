13e469acbd30344173c47aaa3b4b309172ca4e95
Sunp1ant <63898278+Sunp1ant@users.noreply.github.com>
2020-04-18 16:18:40 +0800

    comit

--- bg.js
+++ bg.js
@@ -1,16 +1,13 @@
 (function() {
-    var bodyElement = document.getElementsByTagName("BODY")[0];
     var swap = false;
     var color1 = '#80baf0';
     var color2 = '#fffdf0';
 
     setInterval(function() {
         if (swap){
-            bodyElement.style.backgroundColor = color1;
-            bodyElement.style.color = color1;
+            document.body.style.backgroundColor = color1;
         } else {
-            bodyElement.style.backgroundColor = color2;
-            bodyElement.style.color = color2;
+            document.body.style.backgroundColor = color2;
         }
         swap = !swap
     }, 1000);
