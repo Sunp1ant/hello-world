06d4f3f2d57af8f2a4fde81653a72ad3627c8dfc
Sunp1ant <63898278+Sunp1ant@users.noreply.github.com>
2020-04-18 16:47:54 +0800

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,12 +1,22 @@
 (function() {
     var num = 0;
+    var mode;//false represent decrement, true represents increment
+    
     setInterval(function() {
         document.body.style.backgroundColor = getColor(num);
-        if (num == 4) {
-            num--;
-        } else {
-            num++;
+        if (num == 4){
+            mode = false;
         }
+        if (num == 0){
+            mode = true;
+        }
+        
+        if (mode){
+            num++;
+        } else {
+            num--;
+        }
+        
     }, 500);
 })();
 
